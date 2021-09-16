7bdc2e2c937801ca330033faf7942ec28a7620d2
Joao Paulo Ferreira de Souza <joao.psouza@kroton.com.br>
2021-09-16 17:06:25 -0300

    Cabeçalho novo

--- test.py
+++ test.py
@@ -19,7 +19,7 @@ class Test(unittest.TestCase):
         result = self.app.get('/') 
 
         # verifica o retorno do conteudo da pagina
-        self.assertRegex(result.data.decode(), "JaxStar-DevOps-Essentials-4Linux")
+        self.assertRegex(result.data.decode(), "JaxStar-DevOps-Essentials-4Linux-New")
 
 
 if __name__ == "__main__":
